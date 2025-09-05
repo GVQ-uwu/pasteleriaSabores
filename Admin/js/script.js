1fda91a203af8e2897c92f958db933ce32aefc59
FizzelPopBT <na.quiroz@duocuc.cl>
2025-09-05 23:48:13 +0000

    Se crea el home Admin.

---
+++ Admin/js/script.js
@@ -0,0 +1,8 @@
+// Código JS básico de prueba
+document.addEventListener("DOMContentLoaded", function() {
+  const boton = document.getElementById("saludoBtn");
+
+  boton.addEventListener("click", function() {
+    alert("¡Bienvenido al Panel de Administrador de Pastelería Mil Sabores!");
+  });
+});
